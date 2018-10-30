4afc611cd60bb4ccbd7a6ec2feccce9159d674d7
aillieo <aillieo@163.com>
2018-10-30 23:13:20 +0800

    fix

--- Assets/01_FragWorldPos/Scripts/FragWorldPosImg_01.cs
+++ Assets/01_FragWorldPos/Scripts/FragWorldPosImg_01.cs
@@ -10,12 +10,7 @@ public class FragWorldPosImg_01 : OnRenderImageBase
 
     protected override void ProcessMat(Material mat)
     {
-
-        Matrix4x4 currentViewProjectionMatrix = cam.projectionMatrix * cam.worldToCameraMatrix;
-        Matrix4x4 currentViewProjectionInverseMatrix = currentViewProjectionMatrix.inverse;
-        mat.SetMatrix("_VP_Inverse", currentViewProjectionInverseMatrix);
-
-        //mat.SetMatrix("_VP_Inverse", (cam.projectionMatrix * cam.worldToCameraMatrix).inverse);
+        mat.SetMatrix("_VP_Inverse", (cam.projectionMatrix * cam.worldToCameraMatrix).inverse);
     }
 
 
