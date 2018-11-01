3c12bc38575e6d1464b7e405e9af9162f624d119
aillieo <aillieo@163.com>
2018-11-01 22:56:32 +0800

    add README.md and a script for auto-generation

---
+++ GenerateRM.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+echo start processing ...
+
+#脚本所在路径
+basepath=$(cd `dirname $0`; pwd)
+
+#Assets路径 各文件夹的根目录
+folderoot=$basepath/Assets
+
+rmname="README.md"
+
+folders=()
+
+echo collecting ...
+
+# 获取所有包含README.md的文件夹 保存到数组
+for subpath in $folderoot/*; do
+    if test -d $subpath
+    then
+    	if test -e $subpath/$rmname
+    	then
+    		foldername=${subpath##*/}
+	    	folders=("${folders[@]}" $foldername)
+		fi 
+	fi
+done
+
+filecnt=${#folders[@]}
+echo $filecnt files collected !
+
+# 重置主README.md
+mainfile=$basepath/$rmname
+rm -f $mainfile
+touch $mainfile
+
+echo reset $rmname
+
+# 逆序遍历保存的文件夹 提取REAMME.md
+for(( i=$filecnt;i>=0;i--)) do
+	folder=${folders[i]}
+	filename=$folderoot/$folder/$rmname
+	echo processing ... $filename
+	while read line
+	do
+		# 替换掉 "../../GALLERY" 这样的路径 并将内容追加到主README.md 
+		echo $line | sed -e 's/..\/..\/GALLERY/GALLERY/g' >> $mainfile
+	done < $filename
+	echo '***' >>$mainfile
+done
+
+echo finished!
---
+++ README.md
@@ -0,0 +1,35 @@
+***
+## 01_FragWorldPos
+
+在片段着色器中获取世界坐标，并将坐标作为颜色输出。后处理阶段根据深度纹理解算出来像素对应的世界坐标，可用于做边界检测、运动模糊等效果。
+
+透视相机，直接使用世界坐标绘制物体。
+
+![perspective_0.png](GALLERY/02_HeHeDa/heheda.png)
+***
+## 01_FragWorldPos
+
+在片段着色器中获取世界坐标，并将坐标作为颜色输出。后处理阶段根据深度纹理解算出来像素对应的世界坐标，可用于做边界检测、运动模糊等效果。
+
+透视相机，直接使用世界坐标绘制物体。
+
+![perspective_0.png](GALLERY/01_FragWorldPos/perspective_0.png)
+
+透视相机，在后处理阶段计算世界坐标并显示出来。
+
+![perspective_1.png](GALLERY/01_FragWorldPos/perspective_1.png)
+
+![perspective_2.png](GALLERY/01_FragWorldPos/perspective_2.png)
+
+正交相机，直接使用世界坐标绘制物体。
+
+![orthographic_0.png](GALLERY/01_FragWorldPos/orthographic_0.png)
+
+正交相机，在后处理阶段计算世界坐标并显示出来。
+
+![orthographic_1.png](GALLERY/01_FragWorldPos/orthographic_1.png)
+
+![orthographic_2.png](GALLERY/01_FragWorldPos/orthographic_2.png)
+
+后处理阶段，借助深度纹理获取世界坐标时，使用了两种不同的计算方法，详见相关代码。
+***
